2d1f871ac95080f3727f30b149f72524ada16b58
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2025-04-02 03:21:45 +0400

    Rewrite WWMath::Inv_Sqrt
    Use C++ implementation of Fast Inverse Square Root algorithm to fix
    inability inlining asm code functions.

--- Code/WWMath/wwmath.h
+++ Code/WWMath/wwmath.h
@@ -43,10 +43,11 @@
 #define WWMATH_H
 
 #include "always.h"
-#include <math.h>
-#include <float.h>
-#include <assert.h>
-#include <float.h>
+#include <cassert>
+#include <cfloat>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
 
 /*
 ** Some global constants.
@@ -124,16 +125,15 @@ static WWINLINE int Float_To_Int_Floor(const float& f);
 static WWINLINE float Cos(float val);
 static WWINLINE float Sin(float val);
 static WWINLINE float Sqrt(float val);
-static float __fastcall Inv_Sqrt(float a);	// Some 30% faster inverse square root than regular C++ compiled, from Intel's math library
 static WWINLINE long	 Float_To_Long(float f);
 #else
 static float Cos(float val);
 static float Sin(float val);
 static float Sqrt(float val);
-static float Inv_Sqrt(float a);
 static long	Float_To_Long(float f);
 #endif
 
+static WWINLINE float Inv_Sqrt(float a);
 
 static WWINLINE float Fast_Sin(float val);
 static WWINLINE float Fast_Inv_Sin(float val);
@@ -590,61 +590,26 @@ WWINLINE int WWMath::Float_To_Int_Floor (const float& f)
 }
 
 // ----------------------------------------------------------------------------
-// Inverse square root
+// Fast inverse square root
+// Used adapted C++20 variant (without std::bit_cast)
+// Assuming that sizeof(float) == 4
+// TODO: WH: Probably worth to use faster SSE's rsqrtss instruction
 // ----------------------------------------------------------------------------
 
-#if defined(_MSC_VER) && defined(_M_IX86)
-WWINLINE __declspec(naked) float __fastcall WWMath::Inv_Sqrt(float a)
-{
-	__asm {
-		mov		eax, 0be6eb508h
-		mov		DWORD PTR [esp-12],03fc00000h ;  1.5 on the stack
-		sub		eax, DWORD PTR [esp+4]; a
-		sub		DWORD PTR [esp+4], 800000h ; a/2 a=Y0
-		shr		eax, 1     ; firs approx in eax=R0
-		mov		DWORD PTR [esp-8], eax
-
-		fld		DWORD PTR [esp-8] ;r
-		fmul	st, st            ;r*r
-		fld		DWORD PTR [esp-8] ;r
-		fxch	st(1)
-		fmul	DWORD PTR [esp+4];a ;r*r*y0
-		fld		DWORD PTR [esp-12];load 1.5
-		fld		st(0)
-		fsub	st,st(2)			   ;r1 = 1.5 - y1
-		;x1 = st(3)
-		;y1 = st(2)
-		;1.5 = st(1)
-		;r1 = st(0)
-
-		fld		st(1)
-		fxch	st(1)
-		fmul	st(3),st			; y2=y1*r1*...
-		fmul	st(3),st			; y2=y1*r1*r1
-		fmulp	st(4),st            ; x2=x1*r1
-		fsub	st,st(2)               ; r2=1.5-y2
-		;x2=st(3)
-		;y2=st(2)
-		;1.5=st(1)
-		;r2 = st(0)
-
-		fmul	st(2),st			;y3=y2*r2*...
-		fmul	st(3),st			;x3=x2*r2
-		fmulp	st(2),st			;y3=y2*r2*r2
-		fxch	st(1)
-		fsubp	st(1),st			;r3= 1.5 - y3
-		;x3 = st(1)
-		;r3 = st(0)
-		fmulp	st(1), st
-		ret 4
-	}
-}
-#else
 WWINLINE float WWMath::Inv_Sqrt(float val)
 {
-	return 1.0f / (float)sqrt(val);
-}
-#endif
+	int32_t i;
+	float x2, y;
+	const float threehalfs = 1.5f;
 
+	x2 = val * 0.5f;
+	y  = val;
+	std::memcpy(&i, &y, sizeof(int32_t));
+	i  = 0x5f3759df - (i >> 1);
+	std::memcpy(&y, &i, sizeof(float));
+	y  = y * (threehalfs - (x2 * y * y));		// 1st iteration
+	// y  = y * (threehalfs - (x2 * y * y));	// 2nd iteration, this can be removed
+	return y;
+}
 
 #endif
