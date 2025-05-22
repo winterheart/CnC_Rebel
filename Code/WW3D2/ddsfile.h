2886e985d3f7098ec5b6a1983674295a74afbff9
Azamat H. Hackimov <azamat.hackimov@gmail.com>
2025-05-22 14:45:29 +0300

    Update DDSFileClass

--- Code/WW3D2/ddsfile.h
+++ Code/WW3D2/ddsfile.h
@@ -1,140 +1,59 @@
 /*
-**	Command & Conquer Renegade(tm)
-**	Copyright 2025 Electronic Arts Inc.
-**
-**	This program is free software: you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation, either version 3 of the License, or
-**	(at your option) any later version.
-**
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
-**
-**	You should have received a copy of the GNU General Public License
-**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * 	Command & Conquer Renegade(tm)
+ * 	Copyright 2025 Electronic Arts Inc.
+ * 	Copyright 2025 CnC: Rebel Developers.
+ *
+ * 	This program is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
 
 #ifndef DDSFILE_H
 #define DDSFILE_H
 
-#if defined(_MSC_VER)
-#pragma once
-#endif
+#include <cstdint>
+#include <filesystem>
+#include <vector>
 
 #include "always.h"
 #include "dxdefs.h"
 #include "ww3dformat.h"
-#include "wwstring.h"
 
-// ----------------------------------------------------------------------------
-//
-// This structure represents the old DX7 color key structure. It is needed
-// LegacyDDSURFACEDESC which is needed when loading DDS files. DO NOT MODIFY!
-//
-// ----------------------------------------------------------------------------
-
-struct LegacyDDCOLORKEY {
-  unsigned ColorSpaceLowValue;
-  unsigned ColorSpaceHighValue;
+struct DDS_PIXELFORMAT {
+  uint32_t dwSize;
+  uint32_t dwFlags;
+  uint32_t dwFourCC;
+  uint32_t dwRGBBitCount;
+  uint32_t dwRBitMask;
+  uint32_t dwGBitMask;
+  uint32_t dwBBitMask;
+  uint32_t dwABitMask;
 };
 
-// ----------------------------------------------------------------------------
-//
-// This structure represents the old DX7 CAPS2 structure. It is needed
-// LegacyDDSURFACEDESC which is needed when loading DDS files. DO NOT MODIFY!
-//
-// ----------------------------------------------------------------------------
-
-struct LegacyDDSCAPS2 {
-  unsigned Caps;
-  unsigned Caps2;
-  unsigned Caps3;
-  unsigned Caps4;
-};
-
-// ----------------------------------------------------------------------------
-//
-// This structure represents the old DX7 pixel format structure. It is needed
-// LegacyDDSURFACEDESC which is needed when loading DDS files. DO NOT MODIFY!
-//
-// ----------------------------------------------------------------------------
-
-struct LegacyDDPIXELFORMAT {
-  unsigned Size;
-  unsigned Flags;
-  unsigned FourCC;
-  union {
-    unsigned RGBBitCount;
-    unsigned YUVBitCount;
-    unsigned ZBufferBitDepth;
-    unsigned AlphaBitDepth;
-    unsigned LuminanceBitCount;
-    unsigned BumpBitCount;
-  };
-  union {
-    unsigned RBitMask;
-    unsigned YBitMask;
-    unsigned StencilBitDepth;
-    unsigned LuminanceBitMask;
-    unsigned BumpDuBitMask;
-  };
-  union {
-    unsigned GBitMask;
-    unsigned UBitMask;
-    unsigned ZBitMask;
-    unsigned BumpDvBitMask;
-  };
-  union {
-    unsigned BBitMask;
-    unsigned VBitMask;
-    unsigned StencilBitMask;
-    unsigned BumpLuminanceBitMask;
-  };
-  union {
-    unsigned RGBAlphaBitMask;
-    unsigned YUVAlphaBitMask;
-    unsigned LuminanceAlphaBitMask;
-    unsigned RGBZBitMask;
-    unsigned YUVZBitMask;
-  };
-};
-
-// ----------------------------------------------------------------------------
-//
-// This structure represents the old DX7 surface description structure.
-// It is needed when loading DDS files. DO NOT MODIFY!
-//
-// ----------------------------------------------------------------------------
-
-struct LegacyDDSURFACEDESC2 {
-  unsigned Size;
-  unsigned Flags;
-  unsigned Height;
-  unsigned Width;
-  union {
-    unsigned Pitch;
-    unsigned LinearSize;
-  };
-  unsigned BackBufferCount;
-  union {
-    unsigned MipMapCount;
-    unsigned RefreshRate;
-  };
-  unsigned AlphaBitDepth;
-  unsigned Reserved;
-  void *Surface;
-  union {
-    LegacyDDCOLORKEY CKDestOverlay;
-    unsigned EmptyFaceColor;
-  };
-  LegacyDDCOLORKEY CKDestBlt;
-  LegacyDDCOLORKEY CKSrcOverlay;
-  LegacyDDCOLORKEY CKSrcBlt;
-  LegacyDDPIXELFORMAT PixelFormat;
-  LegacyDDSCAPS2 Caps;
-  unsigned TextureStage;
+struct DDS_HEADER {
+  uint32_t dwSize;
+  uint32_t dwFlags;
+  uint32_t dwHeight;
+  uint32_t dwWidth;
+  uint32_t dwPitchOrLinearSize;
+  uint32_t dwDepth;
+  uint32_t dwMipMapCount;
+  uint32_t dwReserved1[11];
+  DDS_PIXELFORMAT ddspf;
+  uint32_t dwCaps1;
+  uint32_t dwCaps2;
+  uint32_t dwReserved2[3];
 };
 
 // ----------------------------------------------------------------------------
@@ -149,19 +68,19 @@ struct LegacyDDSURFACEDESC2 {
 // ----------------------------------------------------------------------------
 
 class DDSFileClass {
-  unsigned Width;
-  unsigned Height;
-  unsigned FullWidth;
-  unsigned FullHeight;
-  unsigned MipLevels;
+  uint32_t Width;
+  uint32_t Height;
+  uint32_t FullWidth;
+  uint32_t FullHeight;
+  uint32_t MipLevels;
   unsigned long DateTime;
-  unsigned ReductionFactor;
+  uint32_t ReductionFactor;
   unsigned char *DDSMemory;
   WW3DFormat Format;
-  unsigned *LevelSizes;
-  unsigned *LevelOffsets;
-  LegacyDDSURFACEDESC2 SurfaceDesc;
-  char Name[256];
+  std::vector<uint32_t> LevelSizes;
+  std::vector<uint32_t> LevelOffsets;
+  DDS_HEADER SurfaceDesc;
+  std::filesystem::path Name;
 
 public:
   // You can pass the name in .tga or .dds format, the class will automatically try and load .dds file.
@@ -169,15 +88,15 @@ public:
   DDSFileClass(const char *name, unsigned reduction_factor);
   ~DDSFileClass();
 
-  unsigned Get_Width(unsigned level) const;
-  unsigned Get_Height(unsigned level) const;
-  unsigned Get_Full_Width() const { return FullWidth; }   // Get the width of level 0 of non-reduced texture
-  unsigned Get_Full_Height() const { return FullHeight; } // Get the height of level 0 of non-reduced texture
+  uint32_t Get_Width(unsigned level) const;
+  uint32_t Get_Height(unsigned level) const;
+  uint32_t Get_Full_Width() const { return FullWidth; }   // Get the width of level 0 of non-reduced texture
+  uint32_t Get_Full_Height() const { return FullHeight; } // Get the height of level 0 of non-reduced texture
   unsigned long Get_Date_Time() const { return DateTime; }
 
-  unsigned Get_Mip_Level_Count() const { return MipLevels; }
+  uint32_t Get_Mip_Level_Count() const { return MipLevels; }
   const unsigned char *Get_Memory_Pointer(unsigned level) const;
-  unsigned Get_Level_Size(unsigned level) const;
+  uint32_t Get_Level_Size(unsigned level) const;
   WW3DFormat Get_Format() const { return Format; }
 
   // Copy pixels to the destination surface.
@@ -186,7 +105,7 @@ public:
                              unsigned char *dest_surface, unsigned dest_pitch);
 
   // Get pixel in A8R8G8B8 format. This isn't the fastest possible way of reading data from DDS.
-  unsigned Get_Pixel(unsigned level, unsigned x, unsigned y) const;
+  uint32_t Get_Pixel(unsigned level, unsigned x, unsigned y) const;
 
   // Uncompress one 4x4 block from the compressed image.
   // Returns: true if block contained alpha, false is not
@@ -199,9 +118,9 @@ public:
                      unsigned source_y) const; // DDS y offset to copy from, must be aligned by 4!
 
   bool Load();
-  bool Is_Available() const { return !!LevelSizes; }
+  bool Is_Available() const { return !LevelSizes.empty(); }
 
-  static unsigned Calculate_DXTC_Surface_Size(unsigned width, unsigned height, WW3DFormat format);
+  static uint32_t Calculate_DXTC_Surface_Size(unsigned width, unsigned height, WW3DFormat format);
 };
 
 // ----------------------------------------------------------------------------
