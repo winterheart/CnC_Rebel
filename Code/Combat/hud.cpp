winterheart/CnC_Rebel

commit da66fee890e36c2c8c459e214c0183cb5f5809da

Fix compilation warnings in Combat

Code/Combat/hud.cpp
@@ -1,20 +1,21 @@
 /*
-**	Command & Conquer Renegade(tm)
-**	Copyright 2025 Electronic Arts Inc.
-**
-**	This program is free software: you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation, either version 3 of the License, or
-**	(at your option) any later version.
-**
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
-**
-**	You should have received a copy of the GNU General Public License
-**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * 	Command & Conquer Renegade(tm)
+ * 	Copyright 2025 Electronic Arts Inc.
+ * 	Copyright 2025 CnC: Rebel Developers.
+ *
+ * 	This program is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /***********************************************************************************************
  ***                            Confidential - Westwood Studios                              ***
@@ -1919,7 +1920,7 @@ static void Objective_Update(void) {
           dont_clear = true;
 
           // AND, make an extra renderer for the radar star
-          Render2DClass *renderer = new Render2DClass();
+          renderer = new Render2DClass();
           if (renderer) {
             renderer->Set_Texture("HUD_STAR.TGA");
             renderer->Set_Coordinate_Range(Render2DClass::Get_Screen_Resolution());
@@ -1935,7 +1936,7 @@ static void Objective_Update(void) {
             star_fly_end.Y *= 0.8f;
 
             star_box += star_fly_end;
-            Vector2 offset = star_fly_start - star_fly_end;
+            offset = star_fly_start - star_fly_end;
             offset *= fly;
             star_box += offset;
 

Code/Combat/SoundEnvironment.h
@@ -1,20 +1,21 @@
 /*
-**	Command & Conquer Renegade(tm)
-**	Copyright 2025 Electronic Arts Inc.
-**
-**	This program is free software: you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation, either version 3 of the License, or
-**	(at your option) any later version.
-**
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
-**
-**	You should have received a copy of the GNU General Public License
-**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * 	Command & Conquer Renegade(tm)
+ * 	Copyright 2025 Electronic Arts Inc.
+ * 	Copyright 2025 CnC: Rebel Developers.
+ *
+ * 	This program is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /***********************************************************************************************
  ***              C O N F I D E N T I A L  ---  W E S T W O O D  S T U D I O S               ***
@@ -57,7 +58,7 @@ public:
 
   void Reset();
   void Update(PhysicsSceneClass *scene, CameraClass *camera);
-  float Get_Amplitude() { return (AmplitudeSum / AMPLITUDE_BUFFER_SIZE); }
+  float Get_Amplitude() const { return (AmplitudeSum / static_cast<float>(AMPLITUDE_BUFFER_SIZE)); }
 
   void Add_User() { UserCount++; }    // Call to indicate that you want to start using this object.
   void Remove_User() { UserCount--; } // Call to indicate that you no longer need this object.

Code/Combat/WeatherMgr.cpp
@@ -1,20 +1,21 @@
 /*
-**	Command & Conquer Renegade(tm)
-**	Copyright 2025 Electronic Arts Inc.
-**
-**	This program is free software: you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation, either version 3 of the License, or
-**	(at your option) any later version.
-**
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
-**
-**	You should have received a copy of the GNU General Public License
-**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * 	Command & Conquer Renegade(tm)
+ * 	Copyright 2025 Electronic Arts Inc.
+ * 	Copyright 2025 CnC: Rebel Developers.
+ *
+ * 	This program is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /***********************************************************************************************
  ***              C O N F I D E N T I A L  ---  W E S T W O O D  S T U D I O S               ***
@@ -633,9 +634,6 @@ bool WeatherSystemClass::Update(WindClass *wind, const Vector3 &cameraposition) 
 
     if ((Age > 0.0f) && (Can_Spawn(rayptr))) {
 
-      float s;
-      unsigned spawncount;
-
       // Spawn some particles along the ray.
       // NOTE: For accuracy, accumulate fractional spawncounts so that they can be used on a later ray.
       s = ParticlesPerUnitLength * (rayptr->EndPosition - raystartposition).Quick_Length();
@@ -798,7 +796,7 @@ bool WeatherSystemClass::Update(WindClass *wind, const Vector3 &cameraposition) 
 
   // Advance weather system age.
   // NOTE: To prevent floating point overflow, don't advance the age past some maximum.
-  if (Age < MAX_AGE)
+  if (Age < static_cast<float>(MAX_AGE))
     Age += time;
 
   // Is the weather still active (ie. it is contributing to the scene visually or audibly)?

Code/Combat/mapmgr.cpp
@@ -1,20 +1,21 @@
 /*
-**	Command & Conquer Renegade(tm)
-**	Copyright 2025 Electronic Arts Inc.
-**
-**	This program is free software: you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation, either version 3 of the License, or
-**	(at your option) any later version.
-**
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
-**
-**	You should have received a copy of the GNU General Public License
-**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * 	Command & Conquer Renegade(tm)
+ * 	Copyright 2025 Electronic Arts Inc.
+ * 	Copyright 2025 CnC: Rebel Developers.
+ *
+ * 	This program is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /***********************************************************************************************
  ***              C O N F I D E N T I A L  ---  W E S T W O O D  S T U D I O S               ***
@@ -265,11 +266,11 @@ void MapMgrClass::Clear_Cloud_Cells(const Vector3 &pos, int pixel_radius) {
   //
   //	Determine what the min and max cells this region includes
   //
-  int min_cell_x = int(((map_x_pos - pixel_radius) / MapSize.X) * CLOUD_WIDTH);
-  int min_cell_y = int(((map_y_pos - pixel_radius) / MapSize.Y) * CLOUD_HEIGHT);
+  int min_cell_x = int(((map_x_pos - pixel_radius) / MapSize.X) * static_cast<int>(CLOUD_WIDTH));
+  int min_cell_y = int(((map_y_pos - pixel_radius) / MapSize.Y) * static_cast<int>(CLOUD_HEIGHT));
 
-  int max_cell_x = int(((map_x_pos + pixel_radius) / MapSize.X) * CLOUD_WIDTH);
-  int max_cell_y = int(((map_y_pos + pixel_radius) / MapSize.Y) * CLOUD_HEIGHT);
+  int max_cell_x = int(((map_x_pos + pixel_radius) / MapSize.X) * static_cast<int>(CLOUD_WIDTH));
+  int max_cell_y = int(((map_y_pos + pixel_radius) / MapSize.Y) * static_cast<int>(CLOUD_HEIGHT));
 
   //
   //	Clear all the cells in this region

Code/Combat/mapmgr.h
@@ -1,20 +1,21 @@
 /*
-**	Command & Conquer Renegade(tm)
-**	Copyright 2025 Electronic Arts Inc.
-**
-**	This program is free software: you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation, either version 3 of the License, or
-**	(at your option) any later version.
-**
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
-**
-**	You should have received a copy of the GNU General Public License
-**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * 	Command & Conquer Renegade(tm)
+ * 	Copyright 2025 Electronic Arts Inc.
+ * 	Copyright 2025 CnC: Rebel Developers.
+ *
+ * 	This program is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /***********************************************************************************************
  ***              C O N F I D E N T I A L  ---  W E S T W O O D  S T U D I O S               ***
@@ -175,8 +176,8 @@ inline void MapMgrClass::Clear_Cloud_Cell(const Vector3 &pos) {
   //
   //	Convert this map coordinate into a cell coordinate
   //
-  int cell_x = int((map_x_pos / MapSize.X) * CLOUD_WIDTH);
-  int cell_y = int((map_y_pos / MapSize.Y) * CLOUD_HEIGHT);
+  int cell_x = int((map_x_pos / MapSize.X) * static_cast<int>(CLOUD_WIDTH));
+  int cell_y = int((map_y_pos / MapSize.Y) * static_cast<int>(CLOUD_HEIGHT));
 
   //
   //	Clamp the cells
@@ -241,8 +242,8 @@ inline void MapMgrClass::Clear_Cloud_Cell_By_Pixel(int x_pos, int y_pos) {
   //
   //	Convert this map coordinate into a cell coordinate
   //
-  int cell_x = int((x_pos / MapSize.X) * CLOUD_WIDTH);
-  int cell_y = int((y_pos / MapSize.Y) * CLOUD_HEIGHT);
+  int cell_x = int((x_pos / MapSize.X) * static_cast<int>(CLOUD_WIDTH));
+  int cell_y = int((y_pos / MapSize.Y) * static_cast<int>(CLOUD_HEIGHT));
 
   //
   //	Clamp the cells

Code/Combat/objectives.cpp
@@ -1,20 +1,21 @@
 /*
-**	Command & Conquer Renegade(tm)
-**	Copyright 2025 Electronic Arts Inc.
-**
-**	This program is free software: you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation, either version 3 of the License, or
-**	(at your option) any later version.
-**
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
-**
-**	You should have received a copy of the GNU General Public License
-**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * 	Command & Conquer Renegade(tm)
+ * 	Copyright 2025 Electronic Arts Inc.
+ * 	Copyright 2025 CnC: Rebel Developers.
+ *
+ * 	This program is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /***********************************************************************************************
  ***                            Confidential - Westwood Studios                              ***
@@ -409,7 +410,7 @@ void ObjectiveManager::Add_Objective(int id, int type, int status, int short_des
   if (status != ObjectiveManager::STATUS_HIDDEN) {
     WideStringClass message;
     WideStringClass description = TranslateDBClass::Get_String(short_description_id);
-    message.Format(TRANSLATE(IDS_OBJ_NEW_OBJ), objective->Type_To_Name(), description);
+    message.Format(TRANSLATE(IDS_OBJ_NEW_OBJ), objective->Type_To_Name(), description.Peek_Buffer());
     CombatManager::Get_Message_Window()->Add_Message(message, objective->Type_To_Base_Color());
 
     HUDClass::Add_Objective(type);
@@ -489,7 +490,7 @@ void ObjectiveManager::Set_Objective_Status(int id, int status) {
     WideStringClass message;
     if (is_unhiding && (status != ObjectiveManager::STATUS_ACCOMPLISHED)) {
       WideStringClass description = TranslateDBClass::Get_String(objective->ShortDescriptionID);
-      message.Format(TRANSLATE(IDS_OBJ_NEW_OBJ), objective->Type_To_Name(), description);
+      message.Format(TRANSLATE(IDS_OBJ_NEW_OBJ), objective->Type_To_Name(), description.Peek_Buffer());
 
       HUDClass::Add_Objective(objective->Type);
     } else {

Code/Combat/objlibrary.cpp
@@ -1,20 +1,21 @@
 /*
-**	Command & Conquer Renegade(tm)
-**	Copyright 2025 Electronic Arts Inc.
-**
-**	This program is free software: you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation, either version 3 of the License, or
-**	(at your option) any later version.
-**
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
-**
-**	You should have received a copy of the GNU General Public License
-**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * 	Command & Conquer Renegade(tm)
+ * 	Copyright 2025 Electronic Arts Inc.
+ * 	Copyright 2025 CnC: Rebel Developers.
+ *
+ * 	This program is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /***********************************************************************************************
  ***                            Confidential - Westwood Studios                              ***
@@ -54,7 +55,7 @@ PhysicalGameObj *ObjectLibraryManager::Create_Object(int def_id) {
     if (def->Is_Valid_Config(error_message)) {
       return (PhysicalGameObj *)def->Create();
     } else {
-      WWDEBUG_SAY(("Could not create object %s!\n%s\n", def->Get_Name(), error_message));
+      WWDEBUG_SAY(("Could not create object %s!\n%s\n", def->Get_Name(), error_message.Peek_Buffer()));
       return NULL;
     }
   }
@@ -71,7 +72,7 @@ PhysicalGameObj *ObjectLibraryManager::Create_Object(const char *name) {
     if (def->Is_Valid_Config(error_message)) {
       return (PhysicalGameObj *)def->Create();
     } else {
-      WWDEBUG_SAY(("Could not create object %s!\n%s\n", def->Get_Name(), error_message));
+      WWDEBUG_SAY(("Could not create object %s!\n%s\n", def->Get_Name(), error_message.Peek_Buffer()));
       return NULL;
     }
   } else if (def) {

Code/Combat/radar.cpp
@@ -1,20 +1,21 @@
 /*
-**	Command & Conquer Renegade(tm)
-**	Copyright 2025 Electronic Arts Inc.
-**
-**	This program is free software: you can redistribute it and/or modify
-**	it under the terms of the GNU General Public License as published by
-**	the Free Software Foundation, either version 3 of the License, or
-**	(at your option) any later version.
-**
-**	This program is distributed in the hope that it will be useful,
-**	but WITHOUT ANY WARRANTY; without even the implied warranty of
-**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**	GNU General Public License for more details.
-**
-**	You should have received a copy of the GNU General Public License
-**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * 	Command & Conquer Renegade(tm)
+ * 	Copyright 2025 Electronic Arts Inc.
+ * 	Copyright 2025 CnC: Rebel Developers.
+ *
+ * 	This program is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /***********************************************************************************************
  ***                            Confidential - Westwood Studios                              ***
@@ -357,7 +358,7 @@ float RadarManager::Add_Blip(const Vector3 &pos, int shape_type, int color_type,
         if (bracket) {
           color = 0x0000FF00; // Make Green
           color |= (unsigned int)(RadarIntensity * alpha * 255) << 24;
-          RectClass uv = BlipUV[BLIP_BRACKET];
+          uv = BlipUV[BLIP_BRACKET];
           Renderer->Add_Quad(blip, uv, color);
         }
       }

commit f21678336e6159d222b4a5934d9a0441a221bb8d

Fix crash on displaying new objectives
Create new renderer instead of re-use created earlier.
Regression from da66fee8.

Code/Combat/hud.cpp
@@ -1920,10 +1920,10 @@ static void Objective_Update(void) {
           dont_clear = true;
 
           // AND, make an extra renderer for the radar star
-          renderer = new Render2DClass();
-          if (renderer) {
-            renderer->Set_Texture("HUD_STAR.TGA");
-            renderer->Set_Coordinate_Range(Render2DClass::Get_Screen_Resolution());
+          Render2DClass *star_renderer = new Render2DClass();
+          if (star_renderer) {
+            star_renderer->Set_Texture("HUD_STAR.TGA");
+            star_renderer->Set_Coordinate_Range(Render2DClass::Get_Screen_Resolution());
 
             RectClass star_box(-32, -32, 32, 32);
             star_box.Scale(fly);
@@ -1945,8 +1945,8 @@ static void Objective_Update(void) {
               color3 = ObjectiveManager::Get_Objective(index)->Type_To_Color();
             }
             unsigned int color = color3.Convert_To_ARGB();
-            renderer->Add_Quad(star_box, color);
-            ObjectivePogRenderers.Add(renderer);
+            star_renderer->Add_Quad(star_box, color);
+            ObjectivePogRenderers.Add(star_renderer);
           }
 
         } else {